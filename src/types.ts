daf6b8ed3dbf9f639c9e0ea04068236b59a9f899
Emily-lin701 <emilylin701.en12@nycu.edu.tw>
2026-05-21 15:36:33 +0800

    Update NPCCar sprite type to include Rectangle

--- src/types.ts
+++ src/types.ts
@@ -1,8 +1,7 @@
-// src/types.ts
 import Phaser from 'phaser';
 
 export interface NPCCar {
-    sprite: Phaser.GameObjects.Sprite;
+    sprite: Phaser.GameObjects.Rectangle | Phaser.GameObjects.Sprite;
     currentRoadId: string;
     progress: number;
     speed: number;
